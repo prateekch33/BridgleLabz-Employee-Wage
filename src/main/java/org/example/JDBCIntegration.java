b2901eeaf386de90800dfa6ee6e4afcaf9b3af08
prateekch33 <prateekchandra27@outlook.com>
2023-12-17 16:08:53 +0530

    Update Employee Payroll

--- src/main/java/org/example/JDBCIntegration.java
+++ src/main/java/org/example/JDBCIntegration.java
@@ -6,54 +6,53 @@ import java.util.List;
 
 public class JDBCIntegration {
     public Connection connectDatabase() throws SQLException {
-        String url="jdbc:mysql://localhost:3306/payroll_service";
-        String username="root";
-        String password="PrateekC@20035";
-        return DriverManager.getConnection(url,username,password);
+        String url = "jdbc:mysql://localhost:3306/payroll_service";
+        String username = "root";
+        String password = "PrateekC@20035";
+        return DriverManager.getConnection(url, username, password);
     }
 
     public void createTable() throws SQLException {
-        String query="create table employee_payroll(id int auto_increment primary key,name varchar(20),salary float,start_date date, gender char, phone varchar(10), address varchar(200) default 'NA',basic_pay int, deductions int, taxable_pay int, income_tax int, net_pay int, dept_id int after address);";
-        try (Connection connection=connectDatabase();
-            Statement statement=connection.createStatement();) {
+        String query = "create table employee_payroll(id int auto_increment primary key,name varchar(20),salary float,start_date date, gender char, phone varchar(10), address varchar(200) default 'NA',basic_pay int, deductions int, taxable_pay int, income_tax int, net_pay int, dept_id int after address);";
+        try (Connection connection = connectDatabase();
+                Statement statement = connection.createStatement();) {
             statement.executeUpdate(query);
             System.out.println("Table created successfully!!");
-        } catch(SQLException exception) {
+        } catch (SQLException exception) {
             throw new SQLException(exception.getMessage());
         }
     }
 
     public void insertData(Employee ob) throws SQLException {
-        String query="insert into employee_payroll(name,salary,start_date,gender,phone,address,basic_pay,deductions,taxable_pay,income_tax,net_pay,dept_id) values (?,?,?,?,?,?,?,?,?,?,?,?);";
-        try (Connection connection=connectDatabase();
-             PreparedStatement statement=connection.prepareStatement(query)) {
-            statement.setString(1,ob.getName());
-            statement.setDouble(2,ob.getSalary());
-            statement.setString(3,ob.getStart_date());
+        String query = "insert into employee_payroll(name,salary,start_date,gender,phone,address,basic_pay,dept_id,payroll_id) values (?,?,?,?,?,?,?,?,?);";
+        try (Connection connection = connectDatabase();
+                PreparedStatement statement = connection.prepareStatement(query)) {
+            statement.setString(1, ob.getName());
+            statement.setDouble(2, ob.getSalary());
+            statement.setString(3, ob.getStart_date());
             statement.setString(4, String.valueOf(ob.getGender()));
-            statement.setString(5,ob.getPhone());
+            statement.setString(5, ob.getPhone());
             statement.setString(6, ob.getAddress());
-            statement.setInt(7,ob.getBasic_pay());
-            statement.setInt(8,ob.getDeductions());
-            statement.setInt(9,ob.getTaxable_pay());
-            statement.setInt(10,ob.getIncome_tax());
-            statement.setInt(11,ob.getNet_pay());
-            statement.setInt(12,ob.getDept_id());
+            statement.setInt(7, ob.getBasic_pay());
             statement.executeUpdate();
             System.out.println("Data added successfully!!");
-        } catch(SQLException exception) {
+        } catch (SQLException exception) {
             throw new SQLException(exception.getMessage());
         }
     }
 
     public List<Employee> readAllData() throws Exception {
-        List<Employee> employees=new ArrayList<>();
-        String query="select * from employee_payroll";
-        try (Connection connection=connectDatabase();
-            Statement statement=connection.createStatement();
-            ResultSet resultSet=statement.executeQuery(query)) {
+        List<Employee> employees = new ArrayList<>();
+        String query = "select * from employee_payroll";
+        try (Connection connection = connectDatabase();
+                Statement statement = connection.createStatement();
+                ResultSet resultSet = statement.executeQuery(query)) {
             while (resultSet.next()) {
-                Employee ob=new Employee(resultSet.getString("name"),resultSet.getString("start_date"),resultSet.getString("phone"),resultSet.getString("address"),resultSet.getInt("salary"),resultSet.getInt("dept_id"),resultSet.getInt("basic_pay"),resultSet.getInt("deductions"),resultSet.getInt("taxable_pay"),resultSet.getInt("income_tax"),resultSet.getInt("net_pay"),resultSet.getString("gender").charAt(0));
+                Employee ob = new Employee(resultSet.getString("name"), resultSet.getString("start_date"),
+                        resultSet.getString("phone"), resultSet.getString("address"), resultSet.getInt("salary"),
+                        resultSet.getInt("dept_id"), resultSet.getInt("basic_pay"), resultSet.getInt("deductions"),
+                        resultSet.getInt("taxable_pay"), resultSet.getInt("income_tax"), resultSet.getInt("net_pay"),
+                        resultSet.getString("gender").charAt(0));
                 employees.add(ob);
             }
             return employees;
@@ -62,29 +61,32 @@ public class JDBCIntegration {
         }
     }
 
-    public void updateSalary(double salary,String name) throws SQLException {
-        String query="update employee_payroll set salary=? where name=?";
-        try (Connection connection=connectDatabase();
-            PreparedStatement preparedStatement=connection.prepareStatement(query)) {
-            preparedStatement.setDouble(1,salary);
-            preparedStatement.setString(2,name);
+    public void updateSalary(double salary, String name) throws SQLException {
+        String query = "update employee_payroll set salary=? where name=?";
+        try (Connection connection = connectDatabase();
+                PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+            preparedStatement.setDouble(1, salary);
+            preparedStatement.setString(2, name);
             preparedStatement.executeUpdate();
             System.out.println("Salary Updated Successfully!!");
-        } catch(SQLException exception) {
+        } catch (SQLException exception) {
             throw new SQLException(exception.getMessage());
         }
     }
 
     public List<Employee> readSalaryByDate(String date) throws SQLException {
-        List<Employee> employees=new ArrayList<>();
-        String query="select * from employee_payroll where start_date between cast(? as date) and date(now());";
-        try (Connection connection=connectDatabase();
-            PreparedStatement preparedStatement=connection.prepareStatement(query);
-            ) {
-            preparedStatement.setString(1,date);
-            ResultSet resultSet=preparedStatement.executeQuery();
-            while(resultSet.next()) {
-                Employee ob=new Employee(resultSet.getString("name"),resultSet.getString("start_date"),resultSet.getString("phone"),resultSet.getString("address"),resultSet.getInt("salary"),resultSet.getInt("dept_id"),resultSet.getInt("basic_pay"),resultSet.getInt("deductions"),resultSet.getInt("taxable_pay"),resultSet.getInt("income_tax"),resultSet.getInt("net_pay"),resultSet.getString("gender").charAt(0));
+        List<Employee> employees = new ArrayList<>();
+        String query = "select * from employee_payroll where start_date between cast(? as date) and date(now());";
+        try (Connection connection = connectDatabase();
+                PreparedStatement preparedStatement = connection.prepareStatement(query);) {
+            preparedStatement.setString(1, date);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                Employee ob = new Employee(resultSet.getString("name"), resultSet.getString("start_date"),
+                        resultSet.getString("phone"), resultSet.getString("address"), resultSet.getInt("salary"),
+                        resultSet.getInt("dept_id"), resultSet.getInt("basic_pay"), resultSet.getInt("deductions"),
+                        resultSet.getInt("taxable_pay"), resultSet.getInt("income_tax"), resultSet.getInt("net_pay"),
+                        resultSet.getString("gender").charAt(0));
                 employees.add(ob);
             }
             return employees;
@@ -94,25 +96,127 @@ public class JDBCIntegration {
     }
 
     public int getSum(char gender) throws SQLException {
-        String query="select count(salary) as Sum from employee_payroll where gender=?;";
-        try (Connection connection=connectDatabase();
-            PreparedStatement preparedStatement=connection.prepareStatement(query)) {
-            preparedStatement.setString(1,String.valueOf(gender));
-            ResultSet resultSet=preparedStatement.executeQuery();
+        String query = "select count(salary) as Sum from employee_payroll where gender=?;";
+        try (Connection connection = connectDatabase();
+                PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+            preparedStatement.setString(1, String.valueOf(gender));
+            ResultSet resultSet = preparedStatement.executeQuery();
             return resultSet.getInt("Sum");
-        } catch(SQLException exception) {
+        } catch (SQLException exception) {
             throw new SQLException(exception.getMessage());
         }
     }
+
     public int getAverage(char gender) throws SQLException {
-        String query="select avg(salary) as Average from employee_payroll where gender=?;";
-        try (Connection connection=connectDatabase();
-             PreparedStatement preparedStatement=connection.prepareStatement(query)) {
-            preparedStatement.setString(1,String.valueOf(gender));
-            ResultSet resultSet=preparedStatement.executeQuery();
+        String query = "select avg(salary) as Average from employee_payroll where gender=?;";
+        try (Connection connection = connectDatabase();
+                PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+            preparedStatement.setString(1, String.valueOf(gender));
+            ResultSet resultSet = preparedStatement.executeQuery();
             return resultSet.getInt("Average");
-        } catch(SQLException exception) {
+        } catch (SQLException exception) {
             throw new SQLException(exception.getMessage());
         }
+    }
+
+    public void createPayrollDetailsTable() throws SQLException {
+        String query = "CREATE TABLE payroll_details (id INT AUTO_INCREMENT PRIMARY KEY, employee_id INT, deductions FLOAT, taxable_pay FLOAT, tax FLOAT, net_pay FLOAT, FOREIGN KEY (employee_id) REFERENCES employee_payroll(id) ON DELETE CASCADE);";
+        try (Connection connection = connectDatabase();
+                Statement statement = connection.createStatement();) {
+            statement.executeUpdate(query);
+            System.out.println("Payroll details table created successfully with cascading delete!!");
+        } catch (SQLException exception) {
+            throw new SQLException(exception.getMessage());
+        }
+    }
+
+    public void insertDataWithPayrollDetails(Employee ob) throws SQLException {
+        String employeeQuery = "INSERT INTO employee_payroll(name,salary,start_date,gender,phone,address,basic_pay,dept_id,payroll_id) VALUES (?,?,?,?,?,?,?,?,?);";
+        String payrollDetailsQuery = "INSERT INTO payroll_details(employee_id, deductions, taxable_pay, tax, net_pay) VALUES (?,?,?,?,?);";
+        Connection connection = null;
+        PreparedStatement employeeStatement = null;
+        PreparedStatement payrollDetailsStatement = null;
+        try {
+            connection = connectDatabase();
+            connection.setAutoCommit(false);
+            employeeStatement = connection.prepareStatement(employeeQuery, Statement.RETURN_GENERATED_KEYS);
+            employeeStatement.setString(1, ob.getName());
+            employeeStatement.setDouble(2, ob.getSalary());
+            employeeStatement.setString(3, ob.getStart_date());
+            employeeStatement.setString(4, String.valueOf(ob.getGender()));
+            employeeStatement.setString(5, ob.getPhone());
+            employeeStatement.setString(6, ob.getAddress());
+            employeeStatement.setDouble(7, ob.getBasic_pay());
+            employeeStatement.setInt(8, ob.getDept_id());
+            employeeStatement.executeUpdate();
+            ResultSet rs = employeeStatement.getGeneratedKeys();
+            int employeeId = 0;
+            if (rs.next()) {
+                employeeId = rs.getInt(1);
+            }
+            double deductions = ob.getSalary() * 0.2;
+            double taxablePay = ob.getSalary() - deductions;
+            double tax = taxablePay * 0.1;
+            double netPay = ob.getSalary() - tax;
+            payrollDetailsStatement = connection.prepareStatement(payrollDetailsQuery);
+            payrollDetailsStatement.setInt(1, employeeId);
+            payrollDetailsStatement.setDouble(2, deductions);
+            payrollDetailsStatement.setDouble(3, taxablePay);
+            payrollDetailsStatement.setDouble(4, tax);
+            payrollDetailsStatement.setDouble(5, netPay);
+            payrollDetailsStatement.executeUpdate();
+            connection.commit();
+            System.out.println("Data and payroll details added successfully!!");
+        } catch (SQLException exception) {
+            if (connection != null) {
+                try {
+                    connection.rollback();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            throw new SQLException(exception.getMessage());
+        } finally {
+            if (employeeStatement != null) {
+                employeeStatement.close();
+            }
+            if (payrollDetailsStatement != null) {
+                payrollDetailsStatement.close();
+            }
+            if (connection != null) {
+                connection.close();
+            }
+        }
+    }
+
+    public void removeEmployeeFromPayroll(int employeeId) throws SQLException {
+        String query = "UPDATE employee_payroll SET is_active = false WHERE id = ?;";
+        try (Connection connection = connectDatabase();
+                PreparedStatement statement = connection.prepareStatement(query)) {
+            statement.setInt(1, employeeId);
+            statement.executeUpdate();
+            System.out.println("Employee removed from payroll successfully!!");
+        } catch (SQLException exception) {
+            throw new SQLException(exception.getMessage());
+        }
+    }
+
+    public List<Employee> getActiveEmployees() throws SQLException {
+        List<Employee> employeeList = new ArrayList<>();
+        String query = "SELECT * FROM employee_payroll WHERE is_active = true;";
+        try (Connection connection = connectDatabase();
+                PreparedStatement statement = connection.prepareStatement(query);
+                ResultSet resultSet = statement.executeQuery()) {
+            while (resultSet.next()) {
+                Employee employee = new Employee();
+                employee.setName(resultSet.getString("name"));
+                employee.setSalary(resultSet.getDouble("salary"));
+                employee.setGender(resultSet.getString("gender").charAt(0));
+                employeeList.add(employee);
+            }
+        } catch (SQLException exception) {
+            throw new SQLException(exception.getMessage());
+        }
+        return employeeList;
     }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,49 +1,51 @@
 package org.example;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-    static JDBCIntegration jdbc=new JDBCIntegration();
+    static JDBCIntegration jdbc = new JDBCIntegration();
+
     static void addData(Scanner in) {
-        Employee ob=new Employee();
+        Employee ob = new Employee();
         System.out.print("Enter name: ");
-        String name=in.nextLine();
+        String name = in.nextLine();
         ob.setName(name);
         System.out.print("Enter Salary: ");
-        double salary=in.nextFloat();
+        double salary = in.nextFloat();
         ob.setSalary(salary);
         System.out.print("Enter Start Date: ");
-        String date=in.nextLine();
+        String date = in.nextLine();
         ob.setStart_date(date);
         System.out.print("Enter Gender: ");
-        char gender=in.nextLine().charAt(0);
+        char gender = in.nextLine().charAt(0);
         ob.setGender(gender);
         System.out.print("Enter Phone Number: ");
-        String phone=in.nextLine();
+        String phone = in.nextLine();
         ob.setPhone(phone);
         System.out.print("Enter Address: ");
-        String address=in.nextLine();
+        String address = in.nextLine();
         ob.setAddress(address);
         System.out.print("Enter Basic Pay: ");
-        int basic=in.nextInt();
+        int basic = in.nextInt();
         ob.setBasic_pay(basic);
         System.out.print("Enter Deductions: ");
-        int deductions=in.nextInt();
+        int deductions = in.nextInt();
         ob.setDeductions(deductions);
         System.out.print("Enter Taxable Pay: ");
-        int taxable_pay=in.nextInt();
+        int taxable_pay = in.nextInt();
         ob.setTaxable_pay(taxable_pay);
         System.out.print("Enter Income Tax: ");
-        int income_tax=in.nextInt();
+        int income_tax = in.nextInt();
         ob.setIncome_tax(income_tax);
         System.out.print("Enter Net Pay: ");
-        int net_pay=in.nextInt();
+        int net_pay = in.nextInt();
         ob.setNet_pay(net_pay);
         System.out.print("Enter Department Id: ");
-        int dept_id=in.nextInt();
+        int dept_id = in.nextInt();
         ob.setDept_id(dept_id);
         try {
             jdbc.insertData(ob);
@@ -51,63 +53,83 @@ public class Main {
             System.out.println(exception.getMessage());
         }
     }
+
     static void allEmployees() {
         try {
-            List<Employee> allData=jdbc.readAllData();
-            for(Employee employee: allData) {
+            List<Employee> allData = jdbc.readAllData();
+            for (Employee employee : allData) {
                 System.out.println(employee);
             }
         } catch (Exception exception) {
             System.out.println(exception.getMessage());
         }
     }
+
     static void updateSalary(Scanner in) {
         System.out.print("Enter name of the employee: ");
-        String name=in.nextLine();
+        String name = in.nextLine();
         System.out.print("Enter new salary: ");
-        double salary=in.nextDouble();
+        double salary = in.nextDouble();
         try {
-            jdbc.updateSalary(salary,name);
+            jdbc.updateSalary(salary, name);
         } catch (SQLException exception) {
             System.out.println(exception.getMessage());
         }
     }
+
     static void employeesByDate(Scanner in) {
         System.out.print("Enter date: ");
-        String date=in.nextLine();
+        String date = in.nextLine();
         try {
-            List<Employee> allData=jdbc.readSalaryByDate(date);
-            for(Employee employee: allData) {
+            List<Employee> allData = jdbc.readSalaryByDate(date);
+            for (Employee employee : allData) {
                 System.out.println(employee);
             }
         } catch (SQLException exception) {
             System.out.println(exception.getMessage());
         }
     }
+
     static void salarySum() {
         try {
-            int sumMale=jdbc.getSum('M');
-            int sumFemale=jdbc.getSum('F');
-            System.out.println("Sum of salary of all the Male employees: "+sumMale);
-            System.out.println("Sum of salary of all the Female employees: "+sumFemale);
+            int sumMale = jdbc.getSum('M');
+            int sumFemale = jdbc.getSum('F');
+            System.out.println("Sum of salary of all the Male employees: " + sumMale);
+            System.out.println("Sum of salary of all the Female employees: " + sumFemale);
         } catch (SQLException exception) {
             System.out.println(exception.getMessage());
         }
     }
+
     static void salaryAvg() {
         try {
-            int avgMale=jdbc.getAverage('M');
-            int avgFemale=jdbc.getAverage('F');
-            System.out.println("Average of salary of all the Male employees: "+avgMale);
-            System.out.println("Average of salary of all the Female employees: "+avgFemale);
+            int avgMale = jdbc.getAverage('M');
+            int avgFemale = jdbc.getAverage('F');
+            System.out.println("Average of salary of all the Male employees: " + avgMale);
+            System.out.println("Average of salary of all the Female employees: " + avgFemale);
         } catch (SQLException exception) {
             System.out.println(exception.getMessage());
         }
     }
+
+    static void deleteEmployee(Scanner in, Connection connection) {
+        System.out.println("Enter the ID of the employee you want to delete: ");
+        int id = in.nextInt();
+        String sql = "DELETE FROM employees WHERE id = ?";
+
+        try (PreparedStatement pstmt = connection.prepareStatement(sql)) {
+            pstmt.setInt(1, id);
+            pstmt.executeUpdate();
+            System.out.println("Employee deleted successfully.");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
     public static void main(String[] args) {
-        JDBCIntegration ob=new JDBCIntegration();
-        Scanner in=new Scanner(System.in);
-        try (Connection connection=ob.connectDatabase()) {
+        JDBCIntegration ob = new JDBCIntegration();
+        Scanner in = new Scanner(System.in);
+        try (Connection connection = ob.connectDatabase()) {
             System.out.println("Connection Established Successfully!!");
             int choice;
             do {
@@ -118,7 +140,9 @@ public class Main {
                 System.out.println("4. See employees on the basis of Date");
                 System.out.println("5. Sum of Salary of all the employees.");
                 System.out.println("6. Average of Salary of all the employees.");
-                choice=in.nextInt();
+                System.out.println("7. Delete an employee.");
+                System.out.println("8. Exit");
+                choice = in.nextInt();
                 switch (choice) {
                     case 1:
                         addData(in);
@@ -139,14 +163,18 @@ public class Main {
                         salaryAvg();
                         break;
                     case 7:
+                        deleteEmployee(in, connection);
+                        break;
+                    case 8:
+                        System.out.println("Exiting...");
                         break;
                     default:
                         System.out.println("Wrong Choice!!");
                         break;
                 }
-            }while (choice!=7);
-        } catch(SQLException exception) {
-            System.out.println("Exception: "+exception.getMessage());
+            } while (choice != 8);
+        } catch (SQLException exception) {
+            System.out.println("Exception: " + exception.getMessage());
         }
     }
 }
